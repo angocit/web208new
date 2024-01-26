ea25f477f38d8cd58e22ecfd112a43d05108f8d7
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-01-26 09:57:06 +0700

    lession 11

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -11,22 +12,40 @@ import { Router } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private http: HttpClient){
+    
+  }
     loginform = new FormGroup({
-        username: new FormControl('', Validators.required),
-        password: new FormControl('', [Validators.required,Validators.minLength(6)])
+        username: new FormControl('', [Validators.required,Validators.pattern(/^[0-9]d*$/)]),
+        password: new FormControl('', [Validators.required,Validators.pattern(/^0[0-9]{9}$/)])
+        // email: new FormControl('', [Validators.required,Validators.email])
+        // phone: new FormControl('', [Validators.required,Validators.pattern('0+[0-9]+')])
     });
     mess = '';  
     router = inject(Router);  
     onSubmit(){
        let u = this.loginform.controls.username.value;
        let p = this.loginform.controls.password.value
-       if ((u=='admin')&&(p=='123456'))  {
-        this.mess = "Đăng nhập thành công";
-        localStorage.setItem('login','ok');
-        this.router.navigate(['dashboard']);
-       }
-       else {
-        this.mess = "Sai tên đăng nhập hoặc mật khẩu";
-       }
+      //  const data = {username: u, password: p};
+       const body = {username: u,password: p};
+       this.http.post<any>('http://localhost:8000/api/checklogin',body).subscribe((response:any) => {
+          // console.log(response);  
+          if (response.login)  {
+              this.mess = "Đăng nhập thành công";
+              localStorage.setItem('login','ok');
+              this.router.navigate(['dashboard']);
+             }
+             else {
+              this.mess = "Sai tên đăng nhập hoặc mật khẩu";
+             }        
+       });
+      //  if ((u=='admin')&&(p=='123456'))  {
+      //   this.mess = "Đăng nhập thành công";
+      //   localStorage.setItem('login','ok');
+      //   this.router.navigate(['dashboard']);
+      //  }
+      //  else {
+      //   this.mess = "Sai tên đăng nhập hoặc mật khẩu";
+      //  }
     }
 }
